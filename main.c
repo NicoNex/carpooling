c3d0bd789ce2113d32f3b4060b74d184531939a6
NicoNex <nicolo.santamaria@gmail.com>
2019-07-30 15:42:19 +0200

    code cleanup

--- main.c
+++ main.c
@@ -9,10 +9,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#if (defined(_WIN32) || defined(_WIN64))
-#include <_alloca.h>
-#endif
-
 #include "utron/bot.h"
 #include "utron/engine.h"
 #include "utron/dispatcher.h"
